2dde2c14ef743e6302271646ff23f9e849575a68
10xtarun <10xtarun@gmail.com>
2021-05-11 20:07:21 +0530

    graphql schema added

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const {
     graphqlHTTP
 } = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
-
+    schema
 }))
 
 app.listen(5000, (err) => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,8 @@
 const graphql = require('graphql')
 
-const { GraphQLObjectType, GraphQLString } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
+// define and entity and its fields
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
@@ -9,4 +10,24 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
+})
+
+// root query to jump initially in Graph
+const RootQuery = new GraphQLObjectType({
+        name: 'RootQueryType',
+        fields: {
+            book: {
+                type: BookType,
+                args: { id: { type: GraphQLString } },
+                resolve(parent, args) {
+                    // parent is to deal with realtions with other entities
+                    //code to get dta from db
+
+                }
+            }
+        }
+    })
+    // finally creating the Schema 
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
